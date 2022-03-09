ac4a3d4382a29b3bfde7a80cc7f93f3cf4a9ce47
naveen28011610 <naveenku259@gmail.com>
2022-03-09 18:25:51 +0530

    int, prog, replace.py

--- int.py
+++ int.py
@@ -1,3 +1,66 @@
-a=1
-a=int()
-print(a)
+'''n=input("enter the number")
+if int(n[0])%3==0:
+    print("it is divisible")
+else:
+    print("it is not divisibe")'''
+
+
+
+
+# wap to check given iterable is empty or not
+
+'''iterable="54489"
+if (iterable):
+    print("iterable is not empty")
+else:
+    print("iterable is empty")'''
+
+
+
+'''n=1234
+m=str(n)
+if int(m[0])%3==0:
+    print("it is divisible by 3")
+else:
+    print("it is not divisible by 3")'''
+
+
+
+
+
+# wap to print gratest of 3 numbers
+
+'''a=10
+b=20
+c=30
+if (a>b)and (a>c):
+    print("a is gratest of number")
+elif (b>c):
+    print("b is gratest number")
+else:
+    print("c is the gratest number")'''
+    
+    
+'''a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=int(input("enter the number"))
+if  (a>b)and (a>c):
+    print("a is gratest of number")
+elif (b > c):
+    print("b is gratest number")
+else:
+    print("c is the gratest number")'''
+
+
+#convert lowercase to uppercase
+
+
+s=input("enter the charactor")
+res=""
+if ("a"<=s<="z"):
+    res=res+s.upper()
+else:
+    res=res+s
+print(res)
+
+
--- replace.py
+++ replace.py
@@ -1,1 +1,8 @@
-"hello world".replace("world","universe")
+s="today i have playing the football"
+d={}
+for i in range (len(s)):
+    if s[i] not in d:
+        d[s[i]]=[i]
+    else:
+        d[s[i]].append(i)
+print(d)
